f8989d99eb9bd3616390f639e8f62c1d771af15a
Daniel Lee <bearlee@alum.mit.edu>
2011-01-15 00:00:00 +0000

    version 1.0.1

--- R/discrete.histogram.R
+++ R/discrete.histogram.R
@@ -1,6 +1,6 @@
-discrete.histogram <- function (x, prob, prob2=NULL,
+discrete.histogram <- function (x, prob, prob2=NULL, prob3=NULL,
     xlab="x", xaxs.label=NULL, yaxs.label=NULL, bar.width=NULL, 
-    freq=FALSE, prob.col="blue", prob2.col="red", ...){
+    freq=FALSE, prob.col="blue", prob2.col="red", prob3.col="gray", ...){
   if (!missing (x) && missing(prob)) {
 	  prob <- table (x)
 	  x <- sort (unique(x))
@@ -12,6 +12,8 @@ discrete.histogram <- function (x, prob, prob2=NULL,
   
   if (!freq) {
     prob <- prob / sum (prob)
+    prob2 <- prob2 / sum (prob2)
+    prob3 <- prob3 / sum (prob3)
     ylab <- "Probability"  
   } else {
     ylab <- "Count"
@@ -20,12 +22,12 @@ discrete.histogram <- function (x, prob, prob2=NULL,
     x.values <- sort (unique(x))
     n.x.values <- length (x.values)
     
-    gaps <- x.values[2:n.x.values] - x.values[1:(n.x.values-1)]
     if (is.null(bar.width)) {
+      gaps <- x.values[2:n.x.values] - x.values[1:(n.x.values-1)]
       bar.width <- min(gaps)*.2
     }
     par (mar=c(3,3,4,1), mgp=c(1.7,.5,0), tck=-.01)
-    plot (range(x)+c(-1,1)*bar.width, c(0,max(prob)),
+    plot (range(x)+c(-2,2)*bar.width, c(0,max(prob, prob2, prob3)),
         xlab=xlab, ylab=ylab, xaxs="i", xaxt="n",  yaxs="i",
         yaxt=ifelse(is.null(yaxs.label),"s","n"), bty="l", type="n", ...)
     
@@ -35,19 +37,6 @@ discrete.histogram <- function (x, prob, prob2=NULL,
       axis (1, xaxs.label[[1]], xaxs.label[[2]])
     }
     
-    if (!is.null(yaxs.label)){
-      axis (2, yaxs.label[[1]], yaxs.label[[2]])
-    }
-    
-    for (i in 1:length(x)){
-      polygon (x[i] + c(-1,-1,1,1)*bar.width/2, c(0,prob[i],prob[i],0),
-          border=prob.col, col=prob.col)
-      
-      if (!is.null(prob2)) {
-        polygon (x[i] + c(-1,-1,1,1)*bar.width/10, c(0,prob2[i],prob2[i],0),
-            border=prob2.col, col=prob2.col)
-      }
-    }  
   } else {
     x.values <- unique(x)
     n.x.values <- length (x.values)
@@ -56,7 +45,7 @@ discrete.histogram <- function (x, prob, prob2=NULL,
       bar.width <- .2
     }
     par (mar=c(3,3,4,1), mgp=c(1.7,.5,0), tck=-.01)
-    plot (c(1, n.x.values)+c(-1,1)*bar.width, c(0,max(prob)),
+    plot (c(1, n.x.values)+c(-2,2)*bar.width, c(0,max(prob, prob2, prob3)),
         xlab=xlab, ylab=ylab, xaxs="i", xaxt="n",  yaxs="i",
         yaxt=ifelse(is.null(yaxs.label),"s","n"), bty="l", type="n", ...)
     
@@ -65,19 +54,37 @@ discrete.histogram <- function (x, prob, prob2=NULL,
     } else {
       axis (1, xaxs.label[[1]], xaxs.label[[2]])
     }
+       
+    x <- 1:length(x) 
+  }
+  if (!is.null(yaxs.label)){
+    axis (2, yaxs.label[[1]], yaxs.label[[2]])
+  }
+  
+  offset <- rep (NA, 3)
+  if (!is.null (prob2) & !is.null (prob3)) {
+    offset[1] <- -bar.width
+    offset[2] <- 0
+    offset[3] <- bar.width
+  }
+  if (!is.null (prob2) & is.null (prob3)) {
+    offset[1] <- -bar.width/2
+    offset[2] <- bar.width/2
+    offset[3] <- 0
+  }
+  
+  for (i in 1:length(x)){
+    polygon (x[i] + c(-1,-1,1,1)*bar.width/2 + offset[1], c(0,prob[i],prob[i],0),
+        border=prob.col, col=prob.col)
     
-    if (!is.null(yaxs.label)){
-      axis (2, yaxs.label[[1]], yaxs.label[[2]])
+    if (!is.null(prob2)) {
+      polygon (x[i] + c(-1,-1,1,1)*bar.width/2 + offset[2], c(0,prob2[i],prob2[i],0),
+          border=prob2.col, col=prob2.col)
     }
     
-    for (i in 1:length(x)){
-      polygon (i + c(-1,-1,1,1)*bar.width/2, c(0,prob[i],prob[i],0),
-          border=prob.col, col=prob.col)
-      
-      if (!is.null(prob2)) {
-        polygon (i + c(-1,-1,1,1)*bar.width/10, c(0,prob2[i],prob2[i],0),
-            border=prob2.col, col=prob2.col)
-      }
+    if (!is.null(prob3)) {
+      polygon (x[i] + c(-1,-1,1,1)*bar.width/2 + offset[3], c(0,prob3[i],prob3[i],0),
+          border=prob3.col, col=prob3.col)
     }
   }
   
